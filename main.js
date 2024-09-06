9951fd653e6f38fb0239259166b54e9c17b7c935
Razzakh <mrazzakh123@gmail.com>
2024-09-06 18:22:49 +0530

    first

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const fs = require('fs');
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  if(fs.existsSync(filePath)){
+    fs.readFile(filePath,'utf8',(err,data)=>{
+      if(err){
+       return console.error(err);
+      }
+      console.log(data);
+    });
+  }else{
+    console.log(`Column '${filePath}' not found in the CSV`);
+  }
 }
 
 // TODO: Call printFileContents with the command-line argument
+printFileContents('./output.txt')
