d3c5085586b79863c3da1675740f0fb41286af87
Razzakh <mrazzakh123@gmail.com>
2024-09-07 13:04:42 +0530

    norm

--- main.js
+++ main.js
@@ -1,55 +1,26 @@
-// const fs = require('fs');
-// function printFileContents(filePath) {
-//   // TODO: Use fs.readFile to read the file contents
-//   if(fs.existsSync(filePath)){
-//     fs.readFileSync(filePath,'utf8',(err,data)=>{
-//       if(err){
-//        return console.error(err);
-//       }
-//       console.log(data);
-//     });
-//   }else{
-//     console.log(`Column ${filePath} not found in the CSV`);
-//   }
-// }
-// module.exports={printFileContents}
-// // TODO: Call printFileContents with the command-line argument
-// const filePath=process.argv[2];
-// if(filePath){
-//   printFileContents(filePath);
-// }else{
-//   console.log(`Column ${filePath} not found in the CSV`);
-// }
-////////////////////////////////////////////////////////////
-const http = require('http');
 const fs = require('fs');
-const path = require('path');
-
-// Get the file path from the command-line argument
-const filePath = process.argv[2];
-
-// Create the HTTP server
-const server = http.createServer((req, res) => {
-  // Read the file contents
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      // Handle error (file not found or other issues)
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end(`Error: Could not read file. ${err.message}`);
-      return;
-    }
-    // Serve the file contents
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(data);
-  });
-});
-
-// Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-  console.log(`Serving file: ${path.resolve(filePath)}`);
-});
+function printFileContents(filePath) {
+  // TODO: Use fs.readFile to read the file contents
+  if(fs.existsSync(filePath)){
+    fs.readFileSync(filePath,'utf8',(err,data)=>{
+      if(err){
+       return console.error(err);
+      }
+      console.log(data);
+    });
+  }else{
+    console.log(`Column ${filePath} not found in the CSV`);
+  }
+}
+module.exports={printFileContents}
+// TODO: Call printFileContents with the command-line argument
+const filePath=process.argv[2];
+if(filePath){
+  printFileContents(filePath);
+}else{
+  console.log(`Column ${filePath} not found in the CSV`);
+}
+////////////////////////////////////////////////////////////
 
 
 
