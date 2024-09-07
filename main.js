d4238baad55090c5e3d37e7e3f1360d3a6ef8b4a
Razzakh <mrazzakh123@gmail.com>
2024-09-07 12:27:18 +0530

    fsdfsfs

--- main.js
+++ main.js
@@ -1,60 +1,63 @@
-// const fs = require('fs');
-// function printFileContents(filePath) {
-//   // TODO: Use fs.readFile to read the file contents
-//   if(fs.existsSync(filePath)){
-//     fs.readFile(filePath,'utf8',(err,data)=>{
-//       if(err){
-//        return console.error(err);
-//       }
-//       console.log(data);
-//     });
-//   }else{
-//     console.log(`Column ${filePath} not found in the CSV`);
+const fs = require('fs');
+function printFileContents(filePath) {
+  // TODO: Use fs.readFile to read the file contents
+  if(fs.existsSync(filePath)){
+    fs.readFile(filePath,'utf8',(err,data)=>{
+      if(err){
+       return console.error(err);
+      }
+      console.log(data);
+    });
+  }else{
+    console.log(`Column ${filePath} not found in the CSV`);
+  }
+}
+module.exports={printFileContents}
+// TODO: Call printFileContents with the command-line argument
+const filePath=process.argv[2];
+if(filePath){
+  printFileContents(filePath);
+}else{
+  console.log(`Column ${filePath} not found in the CSV`);
+}
+
+
+
+////////////////////////////////////////////////////////////
+// const fs = require("fs");
+
+// function readAndParseJSON(filePath) {
+//   try {
+//     const jsonData = fs.readFileSync(filePath, "utf8");
+//     const parsedData = JSON.parse(jsonData);
+
+//     if (
+//       typeof parsedData.name === "undefined" ||
+//       typeof parsedData.age === "undefined"
+//     ) {
+//       console.log("Missing required data in the JSON file.");
+//       return;
+//     }
+
+//     console.log(JSON.stringify(parsedData));
+//   } catch (error) {
+//     if (error instanceof SyntaxError) {
+//       console.log(
+//         "Invalid JSON file format. Please provide a valid JSON file."
+//       );
+//     } else {
+//       console.log(`Error reading the file: ${error.message}`);
+//     }
 //   }
 // }
 
-// // TODO: Call printFileContents with the command-line argument
-// const filePath=process.argv[2];
-// if(filePath){
-//   printFileContents(filePath);
-// }else{
-//   console.log(`Column ${filePath} not found in the CSV`);
+// if (require.main === module) {
+//   const filePath = process.argv[2];
+//   if (!filePath) {
+//     console.log("Please provide a JSON file path.");
+//     return;
+//   }
+//   readAndParseJSON(filePath);
 // }
-////////////////////////////////////////////////////////////
-const fs = require("fs");
 
-function readAndParseJSON(filePath) {
-  try {
-    const jsonData = fs.readFileSync(filePath, "utf8");
-    const parsedData = JSON.parse(jsonData);
-
-    if (
-      typeof parsedData.name === "undefined" ||
-      typeof parsedData.age === "undefined"
-    ) {
-      console.log("Missing required data in the JSON file.");
-      return;
-    }
-
-    console.log(JSON.stringify(parsedData));
-  } catch (error) {
-    if (error instanceof SyntaxError) {
-      console.log(
-        "Invalid JSON file format. Please provide a valid JSON file."
-      );
-    } else {
-      console.log(`Error reading the file: ${error.message}`);
-    }
-  }
-}
-
-if (require.main === module) {
-  const filePath = process.argv[2];
-  if (!filePath) {
-    console.log("Please provide a JSON file path.");
-    return;
-  }
-  readAndParseJSON(filePath);
-}
-
-module.exports = { readAndParseJSON };
+// module.exports = { readAndParseJSON };
