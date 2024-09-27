9fc0482ceb0f97a1b5e0c892fcbb2ac599ad3a0c
Razzakh <mrazzakh123@gmail.com>
2024-09-27 09:02:58 +0530

    new 1

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function printFileContents(filePath) {
       if(err){
        return console.error(`Error reading file: ${err.message}`);
       }
-      console.log(data);
+     return console.log(data);
     });
   }else{
-    console.log(`Column ${filePath} not found in the CSV`);
+   return console.log(`Column ${filePath} not found in the CSV`);
   }
 }
 module.exports={printFileContents}
@@ -21,38 +21,6 @@ if(filePath){
   console.log(`'Error: No file path provided.'`);
 }
 ////////////////////////////////////////////////////////////
-
-// const fs = require('fs');
-// const path = require('path');
-
-// // Get the file path from the command-line argument
-// const filePath = process.argv[2];
-
-// // Function to read and print file contents
-// const readFileAndPrint = (file) => {
-//   fs.readFile(file, 'utf8', (err, data) => {
-//     if (err) {
-//       // Handle file read errors
-//       if (err.code === 'ENOENT') {
-//         console.error(`Error: Column '${path.basename(file)}' not found in the CSV.`);
-//       } else {
-//         console.error(`Error: Could not read file. ${err.message}`);
-//       }
-//     } else {
-//       // Print file contents to the console
-//       console.log(data);
-//     }
-//   });
-// };
-
-// // Check if the file path is provided
-// if (!filePath) {
-//   console.error('Error: Please provide a file path as a command-line argument.');
-// } else {
-//   // Resolve the file path and read the file
-//   const resolvedFilePath = path.resolve(filePath);
-//   readFileAndPrint(resolvedFilePath);
-// }
 
 
 ////////////////////////////////////////////////////////////
