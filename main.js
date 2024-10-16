f777ad7706b20c4cd612c7403cc82532fd5d3edd
Razzakh <mrazzakh123@gmail.com>
2024-10-16 22:13:03 +0530

    thisfsf

--- main.js
+++ main.js
@@ -31,17 +31,16 @@
 const fs = require("fs");
 const filePath = process.argv[2];
 if (!filePath) {
-  console.log("Usage: node print.js <file_path>");
+  console.error("Usage: node print.js <file_path>");
   process.exit(1);
 }
-fs.readFile(filePath,'utf8', (err, data) => {
+fs.readFile(filePath, "utf8", (err, data) => {
   if (err) {
-    console.log(`Error: ${err.message}`);
+    console.error(`Error: ${err.message}`);
   } else {
     console.log(data);
   }
 });
-
 ////////////////////////////////////////////////////////////
 // const fs = require("fs");
 
